0967fd330c48e457cec747362f39c194b29c06ae
anhle10051996 <88309815+anhle10051996@users.noreply.github.com>
2023-08-08 10:03:09 +0700

    Create api repo (#5)
    * Create api repo
    
    * chore: implement api flow
    
    * chore: update api field
    
    * chore: show toast when call api successfully
    
    * chore: clean repo

---
+++ packages/rn-api/src/components/InputWIthLabel.js
@@ -0,0 +1,39 @@
+import {Text, StyleSheet, View} from 'react-native'
+import React from 'react'
+import {responsiveHeight} from '../themes/metrics'
+import TextInputView from './TextInputView'
+
+const InputWithLabel = props => {
+  const {title, style, onChangeText, value, defaultValue, ...rest} = props
+  return (
+    <View style={[styles.container, style]}>
+      {title && <Text style={styles.title}>{title}</Text>}
+      <TextInputView
+        onChangeText={onChangeText}
+        customStyle={styles.textInputStyle}
+        value={value}
+        defaultValue={defaultValue}
+        {...rest}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: responsiveHeight(10),
+    width: '100%',
+  },
+  title: {
+    marginBottom: responsiveHeight(5),
+    fontSize: responsiveHeight(16),
+    fontWeight: '500',
+  },
+  textInputStyle: {
+    borderWidth: 1,
+    height: responsiveHeight(40),
+    borderRadius: 5,
+  },
+})
+
+export default React.memo(InputWithLabel)
---
+++ packages/rn-api/src/components/TextInputView.js
@@ -0,0 +1,66 @@
+import React, {useRef, forwardRef, useImperativeHandle, memo} from 'react'
+import {TextInput, View, StyleSheet} from 'react-native'
+import {responsiveHeight} from '../themes'
+
+const TextInputView = forwardRef((props, ref) => {
+  const {
+    value,
+    defaultValue,
+    onChangeText,
+    placeholder,
+    onSubmitEditing,
+    editable = true,
+    inputStylesContainer,
+    multiline = false,
+    secureTextEntry,
+    customStyle,
+    testID,
+    ...rest
+  } = props
+
+  const inputRef = useRef()
+
+  useImperativeHandle(ref, () => ({
+    focus: () => {
+      inputRef.current.focus()
+    },
+  }))
+
+  const onChangeTextExtend = text => {
+    onChangeText && onChangeText(text)
+  }
+
+  return (
+    <View style={[styles.container, inputStylesContainer]}>
+      <TextInput
+        testID={testID}
+        ref={inputRef}
+        value={value}
+        style={[styles.textInputStyle, customStyle]}
+        onChangeText={onChangeTextExtend}
+        placeholder={placeholder}
+        underlineColorAndroid="transparent"
+        onSubmitEditing={onSubmitEditing}
+        editable={editable}
+        scrollEnabled={false}
+        multiline={multiline}
+        textAlignVertical={'top'}
+        secureTextEntry={secureTextEntry}
+        defaultValue={defaultValue}
+        {...rest}
+      />
+    </View>
+  )
+})
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+  },
+  textInputStyle: {
+    padding: responsiveHeight(5),
+    width: '100%',
+  },
+})
+
+export default memo(TextInputView)
