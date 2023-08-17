8d369e8cdfb202400b64ec2f714a27ba0f5b9f3c
Anh Le <anh.lehoang@saigontechnology.com>
2023-08-17 11:25:31 +0700

    feat: create rn-api-ts repo

---
+++ packages/rn-api-ts/src/store/reducers/index.ts
@@ -0,0 +1,36 @@
+import {combineReducers} from '@reduxjs/toolkit'
+
+// Reducer Imports
+import app, {appInitialState} from './app'
+import loading from './loading'
+import user, {userInitialState} from './user'
+import {persistReducer} from 'redux-persist'
+import INITIAL_STATE from '../initialState'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+// Reducer Export
+export * from './app'
+export * from './user'
+
+export const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  blacklist: Object.keys(INITIAL_STATE),
+}
+
+const userPersistConfig = {
+  key: 'user',
+  storage: AsyncStorage,
+}
+
+export const InitialState = {
+  user: userInitialState,
+  app: appInitialState,
+}
+
+export default combineReducers({
+  // Reducers
+  user: persistReducer(userPersistConfig, user),
+  app,
+  loading,
+})
